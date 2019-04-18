9cebf9cb18b684bf1c28bb1e1aca647102900bcd
Asyraf Fuyu <aisraft26@gmail.com>
2019-04-18 12:14:25 +0800

    Navbar routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,40 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  public appPages = [
-
+   public appPages = [
+    {
+      title: 'Profile',
+      url: '/profile',
+      icon: 'person'
+    },
+    {
+      title: 'Workout',
+      url: '/workout',
+      icon: 'walk'
+    },
+    {
+      title: 'Alarm',
+      url: '/alarm',
+      icon: 'alarm'
+    },
+    {
+      title: 'Settings',
+      // url: '/settings',
+      icon: 'settings'
+    }
   ];
-
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
     this.initializeApp();
+  }
+    signOut() {
+   // this.router.navigate(['/']);
+   // this.afAuth.auth.signOut().then(() => {
+   //    location.reload();
+   // });
   }
 
   initializeApp() {
